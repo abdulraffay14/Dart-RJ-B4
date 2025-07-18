bb8f8dedb3fe49a80596bf66ae1b82cdd77493a4
abdulraffay14 <abdulraffay9292@gmail.com>
2025-07-18 15:50:02 +0500

    assignmet 3 question 6

---
+++ question6.dart
@@ -0,0 +1,19 @@
+void main() {
+  int marks = 85; 
+
+  if (marks >= 90) {
+    print("Grade A");
+  } 
+  else if (marks >= 80) {
+    print("Grade B");
+  } 
+  else if (marks >= 70) {
+    print("Grade C");
+  } 
+  else if (marks >= 60) {
+    print("Grade D");
+  } 
+  else {
+    print("Grade F");
+  }
+}
