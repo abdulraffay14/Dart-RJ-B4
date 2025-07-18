558a12d5360af36c096ee684aa5ca50dd9c1a739
abdulraffay14 <abdulraffay9292@gmail.com>
2025-07-18 15:57:31 +0500

    assignment 3 question 8

---
+++ question8.dart
@@ -0,0 +1,15 @@
+void main() {
+  int number = 15;
+
+  if (number % 3 == 0 || number % 5 == 0) {
+  
+    print("$number is a multiple of 3 or 5");
+  
+  }
+  
+   else {
+  
+    print("$number is NOT a multiple of 3 or 5");
+  
+  }
+}
