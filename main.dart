605dd4791f2ebcbeae91cee9c54c5ef44c92576d
abdulraffay14 <abdulraffay9292@gmail.com>
2025-07-18 14:42:52 +0500

    assignment 3 question 2

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  int age1 = 22;
+  int age2 = 30;
+
+  if (age1 > age2) {
+    print("Person 1 is older and Person 2 is younger.");
+  } 
+  else if (age2 > age1) {
+    print("Person 2 is older and Person 1 is younger.");
+  }
+   else {
+    print("Both persons are of the same age.");
+  }
+}
+
